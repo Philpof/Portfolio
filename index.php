37a7db3613ce8c97020abd3557ebcefa6ae40bd3
Philippe PERECHODOV <philippe.perechodov@free.fr>
2020-07-07 16:28:23 +0200

    nav + section1 + footer

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+include "header.php";
+?>
+
+<section>
+
+  <div id="section1" class="container-fluid pb-5">
+  <!-- Navbar -->
+  <ul class="nav nav-pills pt-2 pb-5">
+      <a class="nav-link text-light ml-md-3 ml-lg-5" href="index.php#section1">A propos</a>
+      <a class="nav-link text-light ml-md-3 ml-lg-5" href="index.php#section2">Projets</a>
+      <a id="art" class="nav-link text-light ml-md-3 ml-lg-5" href="index.php#section3">Articles</a>
+      <a id="con" class="nav-link text-light ml-md-3 ml-lg-5" href="index.php#section4">Contact</a>
+  </ul>
+
+  <!-- Section 1 A propos-->
+  <div class="row">
+    <img id="photo" class="col-sm-8 col-md-5 col-lg-3 m-5 text-white" src="img/chat.jpeg" alt="Photo ">
+    <div id="qui" class="col-sm-10 col-md-10 col-lg-5 col-xl-5 py-5 px-3 ml-sm-5 ml-lg-0 text-light text-justify">
+      <h3>Qui suis-je ?</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <div class="m-3 row d-flex">
+        <a id="clic" class="mx-3" href="https://github.com/Philpof" target="_blank"><img class="logoGithub" src="img/github.png" alt="GitHub"><img class="logoGithub" src="img/githubRed.png" alt="GitHub"></a>
+        <a id="clic" class="mx-3" href="https://www.linkedin.com/in/philippe-perechodov/" target="_blank"><img class="logoLinkedin" src="img/linkedin.png" alt="LinkedIn"><img class="logoLinkedin" src="img/linkedinRed.png" alt="LinkedIn"></a>
+      </div>
+    </div>
+  </div>
+
+
+  </div>
+
+  <!-- Section 2 Projets-->
+  <div id="section2" class="container-fluid">
+
+
+  </div>
+
+  <!-- Section 3 : Articles -->
+  <div id="section3" class="container-fluid">
+
+
+  </div>
+
+  <!-- Section 4 : Contact-->
+  <div id="section4" class="container-fluid">
+
+
+
+  </div>
+
+  <!-- Footer -->
+  <div id="footer" class="container-fluid text-center text-light pt-3 pb-3">
+    <div>
+      <a href="#section1" class="btn text-light" id="phil">Philippe Perechodov</a>
+    </div>
+    <div class="m-3 row d-flex justify-content-center">
+      <a id="clic" class="mx-3" href="https://github.com/Philpof" target="_blank"><img class="logoGithub" src="img/github.png" alt="GitHub"><img class="logoGithub" src="img/githubRed.png" alt="GitHub"></a>
+      <a id="clic" class="mx-3" href="https://www.linkedin.com/in/philippe-perechodov/" target="_blank"><img class="logoLinkedin" src="img/linkedin.png" alt="LinkedIn"><img class="logoLinkedin" src="img/linkedinRed.png" alt="LinkedIn"></a>
+    </div>
+    <p>© Tous droits réservés - 2020</p>
+
+
+  </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+<?php
+include "footer.php";
