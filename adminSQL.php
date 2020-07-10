6a3853abed815157a6b3aed925a58aa2bd519452
Philippe PERECHODOV <philippe.perechodov@free.fr>
2020-07-10 14:19:47 +0200

    Page adminSQL + début requête SQL

---
+++ adminSQL.php
@@ -0,0 +1,74 @@
+<?php
+include "header.php";
+?>
+
+<section class="container">
+
+  <h1 class="text-center">Page d'administration SQL</h1>
+  <hr>
+  <a href="index.php">Revenir au site</a>
+  <hr>
+
+  <!-- Connection serveur -->
+  <p class="font-weight-bold">Connection à la basse de donnée "portfolio" :</p>
+  <?php
+  $servername = "localhost";
+  $username = "root"; // Sauf si le nom à été modifié
+  $password = ""; // Champs vide car par de MDP
+  $dataBaseName = "portfolio"; // On fait une variable pour le nom de la base de données
+
+  try {
+    $bdd = new PDO("mysql:host=$servername;dbname=$dataBaseName", $username, $password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully / Connexion effectuée avec succès";
+    echo "<hr>";
+  } catch(PDOException $e) {
+    echo "Connection failed / Echec de la connexion : " . $e->getMessage();
+    echo "<hr>";
+  }
+  ?>
+
+
+  <!-- Pour afficher la dernière entrée du champ "contenu" de la base de données -->
+  <p class="font-weight-bold">Dernière entrée du champ "contenu" de la table "propos" :</p>
+  <?php
+  $sql = "SELECT id, contenu FROM propos ORDER BY id DESC LIMIT 1";
+  foreach ($bdd -> query($sql) as $lastPropos) {
+    $test = $lastPropos['contenu'];
+    echo $lastPropos['contenu'] . '<br>';
+  }
+  echo "<hr>";
+  ?>
+
+
+  <!-- Pour afficher toutes les entrées et les champs de la base de données -->
+  <p class="font-weight-bold">Contenu de la table "propos" :</p>
+  <?php
+  $sql = "SELECT * FROM propos";
+  echo "<table class='table table-hover table-striped'>
+          <thead>
+            <tr>
+              <th>id.</th>
+              <th>Titre</th>
+              <th>Date</th>
+              <th>Contenu</th>
+            </tr>
+          </thead>
+          <tbody>";
+  foreach ($bdd -> query($sql) as $row) {
+    echo "<tr><td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['titre'] . "</td>";
+    echo "<td>" . $row['date'] . "</td>";
+    echo "<td>" . $row['contenu'] . "</td></tr>";
+  }
+  echo "</tbody></table><hr>";
+  ?>
+
+
+
+
+</section>
+
+<?php
+include "footer.php";
+?>
--- index.php
+++ index.php
@@ -1,46 +1,84 @@
 <?php
 include "header.php";
+// include "sql.php";
 ?>
 
 <!-- Section 1 A propos-->
-<section>
-  <div id="section1" class="container-fluid pb-5">
+<section id="section1" class="container pb-5">
 
-    <h1 class="text-light" id="titre">Philippe Perechodov</h1>
-    <p id="sstitre" class="text-light">Developpeur Web</p>
+  <!-- Titre -->
+  <h1 id="titre" class="text-light">Philippe Perechodov</h1>
+  <h5 id="sstitre" class="text-light font-italic">- Developpeur Web -</h5>
 
-    <!-- Navbar -->
-    <ul class="nav nav-pills">
-        <a class="nav-link text-light" href="index.php#section1">A propos</a>
-        <a class="nav-link text-light" href="index.php#section2">Projets</a>
-        <a class="nav-link text-light" href="index.php#section3">Articles</a>
-        <a class="nav-link text-light" href="index.php#section4">Contact</a>
-    </ul>
+  <!-- Navbar -->
+  <div class="icon-bar col-sm-10 col-md-8 col-lg-6 d-flex flex-column text-center">
+    <a href="#section1" class="effetBar">A propos</a>
+    <a href="#section2" class="effetBar">Projets & Réalisations</a>
+    <a href="adminSQL.php" class="effetBar">Articles</a>
+    <a href="#section4" class="effetBar">Contact</a>
+  </div>
 
-    <!-- Qui suis-le ?-->
-    <div class="row">
-      <img id="photo" class="col-sm-7 col-md-5 col-lg-2 m-5 text-white" src="img/PhotoCV.png" alt="Photo ">
-      <div id="qui" class="col-sm-10 col-md-10 col-lg-5 col-xl-5 pb-5 px-3 ml-sm-5 ml-lg-0 text-light text-justify">
-        <h3>Qui suis-je ?</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <div class="m-3 row d-flex">
-          <a id="clic" class="mx-3" href="https://github.com/Philpof" target="_blank"><img class="logoGithub" src="img/github.png" alt="GitHub"><img class="logoGithub" src="img/githubRed.png" alt="GitHub"></a>
-          <a id="clic" class="mx-3" href="https://www.linkedin.com/in/philippe-perechodov/" target="_blank"><img class="logoLinkedin" src="img/linkedin.png" alt="LinkedIn"><img class="logoLinkedin" src="img/linkedinRed.png" alt="LinkedIn"></a>
-        </div>
+  <!-- <ul class="nav">
+    <a class="nav-link text-light" href="index.php#section1">A propos</a>
+    <a class="nav-link text-light" href="index.php#section2">Projets & Réalisations</a>
+    <a class="nav-link text-light" href="index.php#section3">Articles</a>
+    <a class="nav-link text-light" href="index.php#section4">Contact</a>
+  </ul> -->
+
+  <!-- Qui suis-le ?-->
+  <div class="row">
+    <img id="photo" class="col-sm-7 col-md-5 col-lg-2 m-5 text-white" src="img/PhotoCV.png" alt="Photo ">
+    <div id="qui" class="col-sm-10 col-md-10 col-lg-5 col-xl-5 pb-5 px-3 ml-sm-5 ml-lg-0 text-light text-justify">
+      <h3>Qui suis-je ?</h3>
+      <p>
+        <!-- Pour afficher la dernière entrée du champ "contenu" de la base de données -->
+        <?php
+          echo $test;
+        ?>
+        </p>
+      <div class="m-3 row d-flex">
+        <a id="clic" class="mx-3" href="https://github.com/Philpof" target="_blank"><img class="logoGithub" src="img/github.png" alt="GitHub"><img class="logoGithub" src="img/githubRed.png" alt="GitHub"></a>
+        <a id="clic" class="mx-3" href="https://www.linkedin.com/in/philippe-perechodov/" target="_blank"><img class="logoLinkedin" src="img/linkedin.png" alt="LinkedIn"><img class="logoLinkedin" src="img/linkedinRed.png" alt="LinkedIn"></a>
       </div>
     </div>
   </div>
+
+  <!-- Ligne séparation -->
+  <div id="progress" class="progress">
+    <div class="progress-bar" id="progressBarA" ></div>
+    <div class="progress-bar" id="progressBarB"></div>
+    <div class="progress-bar" id="progressBarC"></div>
+  </div>
+
 </section>
 
-<div class="progress" id="progress">
-  <div class="progress-bar" id="progressBarA" ></div>
-  <div class="progress-bar" id="progressBarB"></div>
-  <div class="progress-bar" id="progressBarC"></div>
+<!-- Ancienne Section 2 Projets-->
+<!-- <div id="section2" class="container-fluid text-light">
+  <div id="sectTitre" class="row">
+    <p class="col-sm-4 ml-5 mt-5 p-3 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <h2 class="text-right col-sm-7 mt-3 pr-5">Projets & Réalisations</h2>
+  </div>
+
+  <div class="angle one"></div>
+  <div class="angle two"></div>
+  <div class="angle three"></div>
+  <div class="angle four"></div>
+
+  <div id="retour" class="icon-bar col-sm-8 d-flex flex-column container-fluid">
+    <a href="#section1" id="retourText" class="effetBar">Revenir en haut de la page</a>
+  </div>
 </div>
 
-<!-- Section 2 Projets-->
-<div id="section2" class="container-fluid text-light">
-  <h2 class="text-right pr-5 pt-3 ">Projets & Réalisations</h2>
+<div class="progress" id="progress">
+  <div class="progress-bar" id="progressBarD" ></div>
+  <div class="progress-bar" id="progressBarB"></div>
+  <div class="progress-bar" id="progressBarF"></div>
+</div> -->
+
+
+<!-- Section 2 : Projets & Réalisations -->
+<div id="section2" class="container text-light">
+  <h2 class="text-left pl-5 pt-3 ">Projets & Réalisations</h2>
   <div class="projets">
     <div class="box">
       <img src="img/Faun.png">
@@ -55,46 +93,41 @@ include "header.php";
       <span>Explorateur de fichiers</span>
     </div>
     <div class="box">
-      <img src="img/BomberLink.png">
-      <span>BomberLink</span>
+      <a href="noWay.php"><img src="img/BomberLink.png">
+      <span>BomberLink</span></a>
     </div>
   </div>
 </div>
-
 <div class="progress" id="progress">
-  <div class="progress-bar" id="progressBarD" ></div>
+  <div class="progress-bar" id="progressBarA" ></div>
   <div class="progress-bar" id="progressBarE"></div>
   <div class="progress-bar" id="progressBarF"></div>
 </div>
 
-  <!-- Section 3 : Articles -->
-  <div id="section3" class="container-fluid text-light">
-    <h2 class="text-left pl-5 pt-3 ">Articles</h2>
-    <div class="angle one"></div>
-    <div class="angle two"></div>
-    <div class="angle three"></div>
-    <div class="angle four"></div>
-  </div>
-  <div class="progress" id="progress">
-    <div class="progress-bar" id="progressBarG" ></div>
-    <div class="progress-bar" id="progressBarH"></div>
-    <div class="progress-bar" id="progressBarI"></div>
-  </div>
 
-  <!-- Section 4 : Contact-->
-  <div id="section4" class="container-fluid">
+<!-- Section 3 : Articles-->
+
+<!-- Section 4 : Contact-->
+<div id="section4" class="container text-light">
+  <h2 id="contacts" class="text-left pl-5 pt-3 ">Contact</h2>
 
 
-
-  </div>
+</div>
+<div class="progress" id="progress">
+  <div class="progress-bar" id="progressBarB" ></div>
+  <div class="progress-bar" id="progressBarH"></div>
+  <div class="progress-bar" id="progressBarB"></div>
+</div>
 
 
 <!-- Footer -->
 <section>
-  <div id="footer" class="container-fluid text-center text-light pt-3 pb-3">
+  <div id="footer" class="container text-center text-light pt-3 pb-3">
     <div>
-      <a href="#section1" class="btn text-light" id="phil">Philippe Perechodov</a>
-      <p class="pb-3">Developpeur Web</p>
+      <div class="icon-bar col-sm-12 d-flex flex-column container-fluid justify-content-center">
+        <a href="#section1" id="phil" class="effetBar">Philippe Perechodov</a>
+      </div>
+      <p class="pb-3 col-sm-12 text-center font-italic">- Developpeur Web -</p>
     </div>
     <div class="m-3 row d-flex justify-content-center">
       <a id="clic" class="mx-3" href="https://github.com/Philpof" target="_blank"><img class="logoGithub" src="img/github.png" alt="GitHub"><img class="logoGithub" src="img/githubRed.png" alt="GitHub"></a>
