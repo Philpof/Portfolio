cec51e33ed8400b20d7ee6d143fce961acbb51d6
Philpof <philippe.perechodov@free.fr>
2020-07-15 17:00:52 +0200

    Mise en page

--- index.php
+++ index.php
@@ -4,31 +4,24 @@ include "connexion.php";
 ?>
 
 <!-- Section 1 A propos-->
-<section id="section1" class="container pb-5">
+<section id="section1" class="container-fluid">
 
   <!-- Titre -->
-  <h1 id="titre" class="text-light">Philippe Perechodov</h1>
-  <h5 id="sstitre" class="text-light font-italic">- Developpeur Web -</h5>
+  <h1 id="titre" class="text-light col-sm-12">Philippe Perechodov</h1>
+  <h5 id="sstitre" class="text-light font-italic col-sm-12">- Developpeur Web -</h5>
 
   <!-- Navbar -->
-  <div class="icon-bar col-sm-10 col-md-8 col-lg-6 d-flex flex-column text-center">
+  <div id="navbar" class="icon-bar col-sm-12 offset-xl-6 col-xl-6 d-flex flex-column">
     <a href="#section1" class="effetBar">A propos</a>
     <a href="#section2" class="effetBar">Projets & Réalisations</a>
     <a href="adminSQL.php" class="effetBar">Articles</a>
     <a href="#section4" class="effetBar">Contact</a>
   </div>
 
-  <!-- <ul class="nav">
-    <a class="nav-link text-light" href="index.php#section1">A propos</a>
-    <a class="nav-link text-light" href="index.php#section2">Projets & Réalisations</a>
-    <a class="nav-link text-light" href="index.php#section3">Articles</a>
-    <a class="nav-link text-light" href="index.php#section4">Contact</a>
-  </ul> -->
-
   <!-- Qui suis-le ?-->
   <div class="row">
-    <img id="photo" class="col-sm-7 col-md-5 col-lg-2 m-5 text-white" src="img/PhotoCV.png" alt="Photo ">
-    <div id="qui" class="col-sm-10 col-md-10 col-lg-5 col-xl-5 pb-5 px-3 ml-sm-5 ml-lg-0 text-light text-justify">
+    <img id="photo" class="col-sm-7 col-md-5 col-lg-2 text-white" src="img/PhotoCV.png" alt="Photo ">
+    <div id="qui" class="col-sm-10 col-md-10 col-lg-8 ml-sm-5 ml-lg-0 text-light text-justify">
       <h3>Qui suis-je ?</h3>
       <p>
         <!-- SQL : Pour afficher la dernière entrée du champ "contenu" de la base de données -->
@@ -36,7 +29,7 @@ include "connexion.php";
           echo $lastPropos['contenu'];
         ?>
         </p>
-      <div class="m-3 row d-flex">
+      <div class="m-2 row d-flex">
         <a id="clic" class="mx-3" href="https://github.com/Philpof" target="_blank"><img class="logoGithub" src="img/github.png" alt="GitHub"><img class="logoGithub" src="img/githubRed.png" alt="GitHub"></a>
         <a id="clic" class="mx-3" href="https://www.linkedin.com/in/philippe-perechodov/" target="_blank"><img class="logoLinkedin" src="img/linkedin.png" alt="LinkedIn"><img class="logoLinkedin" src="img/linkedinRed.png" alt="LinkedIn"></a>
       </div>
@@ -77,7 +70,7 @@ include "connexion.php";
 
 
 <!-- Section 2 : Projets & Réalisations -->
-<div id="section2" class="container text-light">
+<div id="section2" class="container-fluid text-light">
   <h2 class="text-left pl-5 pt-3 ">Projets & Réalisations</h2>
   <div class="projets">
     <div class="box">
@@ -92,46 +85,51 @@ include "connexion.php";
       <img src="img/Explorateur.png">
       <span>Explorateur de fichiers</span>
     </div>
-    <div class="box">
-      <a href="noWay.php"><img src="img/BomberLink.png">
-      <span>BomberLink</span></a>
-    </div>
+    <a href="noWay.php" class="box">
+      <img src="img/BomberLink.png">
+      <span>BomberLink</span>
+    </a>
   </div>
-</div>
-<div class="progress" id="progress">
-  <div class="progress-bar" id="progressBarA" ></div>
-  <div class="progress-bar" id="progressBarE"></div>
-  <div class="progress-bar" id="progressBarF"></div>
+
+  <div class="progress" id="progress">
+    <div class="progress-bar" id="progressBarA" ></div>
+    <div class="progress-bar" id="progressBarE"></div>
+    <div class="progress-bar" id="progressBarF"></div>
+  </div>
+
 </div>
 
 
 <!-- Section 3 : Articles-->
 
 <!-- Section 4 : Contact-->
-<div id="section4" class="container text-light">
+<div id="section4" class="container-fluid text-light">
   <h2 id="contacts" class="text-left pl-5 pt-3 ">Contact</h2>
 
 
-</div>
-<div class="progress" id="progress">
-  <div class="progress-bar" id="progressBarB" ></div>
-  <div class="progress-bar" id="progressBarH"></div>
-  <div class="progress-bar" id="progressBarB"></div>
+  <div class="progress" id="progress">
+    <div class="progress-bar" id="progressBarB" ></div>
+    <div class="progress-bar" id="progressBarH"></div>
+    <div class="progress-bar" id="progressBarB"></div>
+  </div>
+
 </div>
 
 
 <!-- Footer -->
 <section>
-  <div id="footer" class="container text-center text-light pt-3 pb-3">
+  <div id="footer" class="container-fluid text-center text-light pt-3 pb-3">
     <div>
-      <div class="icon-bar col-sm-12 d-flex flex-column container-fluid justify-content-center">
+      <div class="icon-bar no-wrap col-sm-12 d-flex flex-column container-fluid justify-content-center">
         <a href="#section1" id="phil" class="effetBar">Philippe Perechodov</a>
       </div>
       <p class="pb-3 col-sm-12 text-center font-italic">- Developpeur Web -</p>
+      <p class="pb-3 col-sm-12 text-center ">06 26 64 84 91</p>
+      <a href="mailto:philippe.perechodov@free.fr" class="text-light">philippe.perechodov@free.fr</a>
     </div>
     <div class="m-3 row d-flex justify-content-center">
-      <a id="clic" class="mx-3" href="https://github.com/Philpof" target="_blank"><img class="logoGithub" src="img/github.png" alt="GitHub"><img class="logoGithub" src="img/githubRed.png" alt="GitHub"></a>
-      <a id="clic" class="mx-3" href="https://www.linkedin.com/in/philippe-perechodov/" target="_blank"><img class="logoLinkedin" src="img/linkedin.png" alt="LinkedIn"><img class="logoLinkedin" src="img/linkedinRed.png" alt="LinkedIn"></a>
+      <a id="clic" class="m-3" href="https://github.com/Philpof" target="_blank"><img class="logoGithub" src="img/github.png" alt="GitHub"><img class="logoGithub" src="img/githubRed.png" alt="GitHub"></a>
+      <a id="clic" class="m-3" href="https://www.linkedin.com/in/philippe-perechodov/" target="_blank"><img class="logoLinkedin" src="img/linkedin.png" alt="LinkedIn"><img class="logoLinkedin" src="img/linkedinRed.png" alt="LinkedIn"></a>
     </div>
     <p>© Tous droits réservés - 2020</p>
   </div>
